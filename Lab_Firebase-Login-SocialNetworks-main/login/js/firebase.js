d36d1e0862ffee0808984afc47ade747724b6d35
Nelson Tovar <ntovar@fiec.espol.edu.ec>
2024-10-07 00:58:24 -0700

    First commit

---
+++ Lab_Firebase-Login-SocialNetworks-main/login/js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXfxsEw9NnwKAbXgpY54YBSW4nvKEajRw",
+  authDomain: "socialnetworkproject-e0599.firebaseapp.com",
+  projectId: "socialnetworkproject-e0599",
+  storageBucket: "socialnetworkproject-e0599.appspot.com",
+  messagingSenderId: "25609857269",
+  appId: "1:25609857269:web:a6d52ff12f323ff5bf3dbc"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
---
+++ js/firebase.js
@@ -0,0 +1,25 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnqIbHUyMikazZN46r3iDiMTDSJIxjghs",
+  authDomain: "portfolio-v1-nelson-tovar.firebaseapp.com",
+  projectId: "portfolio-v1-nelson-tovar",
+  storageBucket: "portfolio-v1-nelson-tovar.appspot.com",
+  messagingSenderId: "161553090990",
+  appId: "1:161553090990:web:129d080e830700b87a603f",
+  measurementId: "G-JK10DRS0KE"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
